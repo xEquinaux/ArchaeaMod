9422ebcb426e35ec324e303db8468c28eb07d962
Nolan Flammini <nolantheturtle@gmail.com>
2024-04-08 15:38:41 -0700

    [init] * Added a form and Xna surface * StartType enum

---
+++ Args.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArchaeaMod
+{
+	public enum StartType : int
+	{
+		/// <summary>
+		/// Start form control only.
+		/// </summary>
+		None = -1,
+		/// <summary>
+		/// Start the form and surface controls.
+		/// </summary>
+		Normal = 0,
+		/// <summary>
+		/// Start only the surface control.
+		/// </summary>
+		Debug = 1,
+		/// <summary>
+		/// Unused / same as None start type.
+		/// </summary>
+		Custom = 2
+	}
+	public static class Arg
+	{
+		static string[] Args;
+		static StartType Type;
+		public static void LaunchProperties(string[] args, StartType type)
+		{
+			Args = args;
+			Type = type;
+			switch (type)
+			{
+				case StartType.Custom:
+				case StartType.None:
+				default:
+					Application.EnableVisualStyles();
+					Application.SetCompatibleTextRenderingDefault(false);
+					Application.Run(new Overlay());
+					break;
+				case StartType.Debug:
+					using (Production.Instance = new Production())
+					{ 
+						Production.Instance.Window.IsBorderless = true;
+						Production.Instance.Window.AllowAltF4 = true;
+						Production.Instance.Window.AllowUserResizing = false;
+						Production.Instance.Run();
+					}
+					break;
+				case StartType.Normal:
+					Application.EnableVisualStyles();
+					Application.SetCompatibleTextRenderingDefault(false);
+					Application.Run(new Overlay());
+					using (Production.Instance = new Production())
+					{ 
+						Production.Instance.Window.IsBorderless = true;
+						Production.Instance.Window.AllowAltF4 = true;
+						Production.Instance.Window.AllowUserResizing = false;
+						Production.Instance.Run();
+					}
+					break;
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,61 @@
-﻿
-using var game = new ArchaeaMod.Game1();
-game.Run();
+﻿using ArchaeaMod;
+using Microsoft.Xna.Framework;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+internal partial class Overlay : Form
+{
+	[STAThread]
+	static void Main(string[] args)
+	{
+		Arg.LaunchProperties(args, StartType.Debug);
+	}
+
+	static Overlay Instance;
+	Production instance => ArchaeaMod.Production.Instance;
+
+	public Overlay()
+	{
+		this.FormBorderStyle = FormBorderStyle.None;
+		this.ShowInTaskbar = false;
+		this.Load		+= new EventHandler(Overlay_Load);
+		this.Paint		+= new PaintEventHandler(Overlay_Paint);
+		this.Move		+= new EventHandler(Overlay_Move);
+		this.Resize		+= new EventHandler(Overlay_Resize);
+	}
+
+	private static void Game_Exiting(object sender, EventArgs e)
+	{
+		Instance.Close();
+	}
+	 
+	private void Overlay_Resize(object sender, EventArgs e)
+	{
+		instance._graphics.PreferredBackBufferWidth = this.Width;
+		instance._graphics.PreferredBackBufferHeight = this.Height;
+		instance._graphics.ApplyChanges();
+	}
+
+	private void Overlay_Move(object sender, EventArgs e)
+	{
+		instance.Window.Position = new Microsoft.Xna.Framework.Point(this.Location.X, this.Location.Y);
+	}
+
+	private void Overlay_Load(object sender, EventArgs e)
+	{
+		this.TopMost = true;
+		this.Location = new System.Drawing.Point(0, 0);
+		this.Size = new Size(800, 600);
+		this.TransparencyKey = this.BackColor = System.Drawing.Color.Red;
+	}
+
+	private void Overlay_Paint(object sender, PaintEventArgs e)
+	{
+		e.Graphics.DrawString("Hello, Overlay!", new Font("Arial", 16), Brushes.Black, new PointF(10, 10));
+	}
+
+	private void InitializeComponent()
+	{
+	}
+}
